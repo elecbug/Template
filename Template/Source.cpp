59b3b2b2c1e015c30e9f9949867863377459346b
deveb1479@gmail.com <deveb1479@gmail.com>
2022-11-11 16:11:37 +0900

    add behavior

--- Template/Source.cpp
+++ Template/Source.cpp
@@ -7,22 +7,42 @@ void test()
 	EB::List<int> list = EB::List<int>(7);
 
 	for (int i = 0; i < 10; i++)
-		list.insert(0, i);
+		list.insert(list.count(), i);
 
 	for (int i = 0; i < list.count(); i++)
 		std::cout << list.at(i) << " ";
+	std::cout << std::endl;
 
 	list.remove(1);
 
 	for (int i = 0; i < list.count(); i++)
 		std::cout << list.at(i) << " ";
+	std::cout << std::endl;
 
 	list.insert(list.count(), -1);
 	list.at(0) = 8;
 
 	for (int i = 0; i < list.count(); i++)
 		std::cout << list.at(i) << " ";
+	std::cout << std::endl;
 
+	std::cout << list.index_of(-1, [](int x, int y)->bool { return x == y; }) << std::endl;
+	std::cout << list.index_of(8) << std::endl;
+
+	EB::List<int> list2 = EB::List<int>(9, 10);
+	list2.add(30);
+	
+	list2.append(list);
+
+	for (int i = 0; i < list2.count(); i++)
+		std::cout << list2.at(i) << " ";
+	std::cout << std::endl;
+
+	list = list2.sublist(5, 15);
+
+	for (int i = 0; i < list.count(); i++)
+		std::cout << list.at(i) << " ";
+	std::cout << std::endl;
 }
 
 int main()
--- Template/list.cpp
+++ Template/list.cpp
@@ -41,7 +41,7 @@ namespace EB
 
 		Node*& get(size_t index)
 		{
-			if (index >= 0 && index <= this->size)
+			if (index >= 0 && index <= this->size / 2)
 			{
 				Node* result = this->head->next();
 
@@ -52,14 +52,24 @@ namespace EB
 
 				return result;
 			}
+			else if (index > this->size / 2 && index <= this->size)
+			{
+				Node* result = this->tail;
+
+				for (int i = this->size; i > index; i--)
+				{
+					result = result->previous();
+				}
+
+				return result;
+			}
 			else
 			{
 				throw "OutOfIndex";
 			}
 		}
 
-	public:
-		List(size_t count = 0)
+		void initailize()
 		{
 			this->head = new Node();
 			this->tail = new Node();
@@ -68,10 +78,25 @@ namespace EB
 			this->tail->previous() = this->head;
 
 			this->size = 0;
+		}
+
+	public:
+		List(size_t count = 0, T init_value = 0)
+		{
+			initailize();
 
 			for (int i = 0; i < count; i++)
 			{
-				insert(0, 0);
+				insert(0, init_value);
+			}
+		}
+		List(T* data_array, size_t length)
+		{
+			initailize();
+
+			for (int i = 0; i < length; i++)
+			{
+				insert(i, data_array[i]);
 			}
 		}
 
@@ -122,6 +147,48 @@ namespace EB
 		size_t count()
 		{
 			return this->size;
+		}
+
+		size_t index_of(T data, bool (*condition)(T x, T y) = [](T x, T y)->bool {return x == y; })
+		{
+			Node* temp = this->head->next();
+
+			for (int i = 0; i < count(); i++)
+			{
+				if (condition(temp->value(), data)) 
+				{
+					return i;
+				}
+				else
+				{
+					temp = temp->next();
+				}
+			}
+		}
+
+		void append(List<T>& post)
+		{
+			for (int i = 0; i < post.count(); i++)
+			{
+				insert(count(), post.at(i));
+			}
+		}
+
+		List<T>& sublist(size_t start, size_t end)
+		{
+			List<T>* result = new List(0);
+
+			for (int i = start; i < end; i++)
+			{
+				result->insert(i - start, at(i));
+			}
+
+			return *result;
+		}
+
+		void add(T data)
+		{
+			insert(count(), data);
 		}
 	};
 }
